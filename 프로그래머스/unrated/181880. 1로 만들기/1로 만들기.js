393ad84e17fa88189aaa0d0db31dbfa1b76c79c3
hgyeom <96222942+hgyeom@users.noreply.github.com>
2023-10-10 17:57:56 +0900

    [level unrated] Title: 1로 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -2,20 +2,9 @@ function solution(num_list) {
     let answer = 0;
     for(let i = 0; i < num_list.length; i++){
         let num = num_list[i];
-        for (let j = 0; j < num_list[i]; j++) {
-            
-            if (num === 1) {
-                answer += j;
-                break;
-            }
-            
-            if (num % 2 === 0) {
-                num = num / 2;
-            }
-            
-            else if (num % 2 === 1) {
-                num = (num - 1) / 2;
-            }
+        while(num > 1){
+            num = num % 2 === 0 ? num / 2 : ( num - 1 ) / 2;
+            answer++
         }
     }
     return answer;
