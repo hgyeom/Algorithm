da58ebefb91ac33ee508e84e2486882ec7481529
hgyeom <96222942+hgyeom@users.noreply.github.com>
2023-10-16 14:32:31 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,8 @@
+function solution(myString, pat) {
+    var answer = 0;
+    for(let i = 0; i < myString.length; i++){
+        let target = myString.slice(i, i+pat.length)
+        if(target === pat) answer++
+    }
+    return answer;
+}
