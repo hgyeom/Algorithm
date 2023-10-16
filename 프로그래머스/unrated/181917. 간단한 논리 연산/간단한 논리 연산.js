8a466dd356916e4ec0f81287b64a8913bb3b3350
hgyeom <96222942+hgyeom@users.noreply.github.com>
2023-10-16 14:58:14 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -1,11 +1,12 @@
 function solution(x1, x2, x3, x4) {
-    let first = (a, b) => {
-        return a || b ? true : false
-    };
+//     let first = (a, b) => {
+//         return a || b ? true : false
+//     };
     
-    let second = (a, b) => {
-        return a && b ? true : false
-    };
+//     let second = (a, b) => {
+//         return a && b ? true : false
+//     };
     
-    return second(first(x1,x2),first(x3,x4));
+//     return second(first(x1,x2),first(x3,x4));
+    return (x1||x2) && (x3||x4)
 }
