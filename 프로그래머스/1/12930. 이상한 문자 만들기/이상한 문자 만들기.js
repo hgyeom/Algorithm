a9c188e0af0fc249c40a8f007fb57d115cd5c1d5
hgyeom <96222942+hgyeom@users.noreply.github.com>
2023-11-14 13:56:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -7,6 +7,7 @@
 
 
 function solution(s) {
+    // map함수는 함수를 받아서 실행하기 때문에 changeLetterCase를 바로 집어넣어도 무방하다.
     const changeLetterCase = (item, index) => index % 2 === 0 ? item.toUpperCase() : item.toLowerCase();
-    return s.split(' ').map(word => word.split('').map(changeLetterCase).join('')).join(' ');
+    return s.split(' ').map(word => word.split('').map(changeLetterCase).join('')).join(' ')
 }
